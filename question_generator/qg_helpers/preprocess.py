7ba5618cb5b20acf66acb7fbda6498f828e74918
Gaurav Shegokar <gaurao2009@gmail.com>
2019-11-12 04:12:20 -0800

    titles removed from the input text as preprocessing part

--- question_generator/qg_helpers/preprocess.py
+++ question_generator/qg_helpers/preprocess.py
@@ -1,5 +1,23 @@
 import re
+import sys
+sys.path.append("../..")
+import util_service
 
+def remove_titles(text):
+    lines = text.splitlines()
+    new_lines = []
+    for line in lines:
+        if len(line.split())<4:
+            # "discarding on length basis"
+            continue
+        pos_tokens = util_service.get_pos_tokens_only(line)
+        if "VERB" not in pos_tokens:
+            # "discarding on grammer basis"
+            continue
+        new_lines.append(line)
+    
+    return "\n".join(new_lines)
+    
 def clean_text(text):
     """
     removes non ascii keywords and content string inside along with brackets
@@ -14,9 +32,11 @@ def clean_text(text):
     
     text = re.sub(r'\([^)]*\)', '', text)
     text = re.sub(r' +', ' ', text)
+    
+    text = remove_titles(text)
     return text
 
 if __name__ == "__main__":
-    with open('/Users/gauravshegokar/Documents/CMU/FALL_2019/NLP/project/Wiki-QA-Magic/data/Development_data/set1/a1.txt', 'r') as content_file:
+    with open('/Users/gauravshegokar/Documents/CMU/FALL_2019/NLP/project/Wiki-QA-Magic/data/Development_data/set1/a2.txt', 'r') as content_file:
         content = content_file.read()
     print(clean_text(content))
