8f478a43dcdeb3da097ae93d757295bb2967489e
Evan Kaaret <evankaaret@Evans-MacBook-Air.local>
2019-11-26 15:27:41 -0500

    Rnadom other answer and correct answer

--- question_generator/qtype_handlers/eo_generator.py
+++ question_generator/qtype_handlers/eo_generator.py
@@ -1,16 +1,11 @@
+import re
 import sys
 sys.path.append("../..")
 import util_service
+import random
 
 from question_generator.qtype_handlers.get_is_are_was_were_loc import which_acomp
 
-
-def get_diff_date(date):
-    # July 2, 1989
-    # 1989
-    year = date[len(date)-4:len(date)]
-    year = int(year) + 4
-    return str(year)
 
 def get_is_idx_from_ner(ner_tags):
     for idx, entry in enumerate(ner_tags):
@@ -24,8 +19,41 @@ def get_was_idx_from_ner(ner_tags):
             return idx
     return -1
 
+def get_diff_date(date):
+    # July 2, 1989
+    # 1989
+    year = date[len(date)-4:len(date)]
+    year = int(year) + 4
+    return str(year)
+
+def get_random_name():
+    possible_names = ["Bill Nye", "Jesus", "Edward Scissorhands", "Adolf Hitler", "Aang", "Anakin Skywalker"]
+    name_index = random.randint(0, len(possible_names)-1)
+    return possible_names[name_index]
+
+def get_random_gpe():
+    possible_names = ["America", "France", "Japan", "Pittsburgh", "Mexico", "Republic City", "New Jersey"]
+    name_index = random.randint(0, len(possible_names)-1)
+    return possible_names[name_index]
+
+def get_random_loc():
+    possible_names = ["the Atlantic Ocean", "Frick Park", "Point State Park", "Flagstaff Hill"]
+    name_index = random.randint(0, len(possible_names)-1)
+    return possible_names[name_index]
+
+def get_random_org():
+    possible_names = ["Apple Inc.", "Amazon", "Duolingo", "Doctors without Borders"]
+    name_index = random.randint(0, len(possible_names)-1)
+    return possible_names[name_index]
+
+def get_random_number():
+    return random.randint(0, 100)
+
 
 def generate_question(sentence):
+    # print("ORIGINAL SENTENCE: ", sentence)
+    # ner_only = util_service.get_ner(sentence)
+    # print(ner_only)
     try:
         is_idx = -1
         was_idx = -1
@@ -33,13 +61,18 @@ def generate_question(sentence):
         sent_tokens = sentence.split()
 
         # get index of is
-        try:
-            is_idx = sent_tokens.index("is")
-            was_idx = sent_tokens.index("was")
+        try: is_idx = sent_tokens.index("is")
         except: pass
+
+        try: was_idx = sent_tokens.index("was")
+        except: pass
+        # print("Passed try except")
+        # print(is_idx)
+        # print(was_idx)
 
         # getting the end of question
         if is_idx != -1:
+            # print("is idx")
             passed_tokens = []
             for i, token in enumerate(sent_tokens):
                 if i > is_idx:
@@ -47,8 +80,9 @@ def generate_question(sentence):
                         if token in ["of"]:
                             continue
                     passed_tokens.append(token)
-                    
+
         elif was_idx != -1:
+            # print("was idx")
             passed_tokens = []
             for i, token in enumerate(sent_tokens):
                 if i > was_idx:
@@ -57,36 +91,73 @@ def generate_question(sentence):
                             continue
                     passed_tokens.append(token)
 
+        # print("Replacing last token")
+        # print(passed_tokens)
+
+        # replaces . with ? if it is last token or it is a part of last token
+        if passed_tokens[-1] == ".":
+            passed_tokens[-1] = "?"
+        elif passed_tokens[-1].endswith("."):
+            passed_tokens[-1] = re.sub("(.*)\.", "\\1?", passed_tokens[-1])
+        else:
+            passed_tokens.append("?")
+
+        # print("Doing ner_tags")
+
         ner_tags = util_service.get_ner_per_token(sentence)
+        # print("doing ner_only")
+        ner_only = util_service.get_ner(sentence)
+        # print("doing is_idx")
         is_idx_ner = get_is_idx_from_ner(ner_tags)
+        # print("Doing was_idx")
         was_idx_ner = get_was_idx_from_ner(ner_tags)
-        acomp_idx_ner = max(is_idx_ner, was_idx_ner)
+
+        # print("substance_of_sent now")
+
+        substance_of_sent = " ".join(passed_tokens)
+
+        # print(ner_only)
+
+        # print(is_idx_ner)
+        # print(was_idx_ner)
+        acomp_idx_ner = -1
+        if is_idx_ner > was_idx_ner:
+            acomp_idx_ner = is_idx_ner
+            acomp_word = "Is"
+        else:
+            acomp_idx_ner = was_idx_ner
+            acomp_word = "Was"
+        # acomp_idx_ner = max(is_idx_ner, was_idx_ner)
         if acomp_idx_ner != -1:
-            if (ner_tags[acomp_idx_ner - 1][1] == "ORG"
-                or ner_tags[acomp_idx_ner - 1][1] == "GPE"):
-                # q_type = "What"
-                wrong = "Apple Inc."
+            # print(ner_tags[acomp_idx_ner - 1][1])
+            if ner_tags[acomp_idx_ner - 1][1] == "ORG":
+                wrong = get_random_org()
+            elif ner_tags[acomp_idx_ner - 1][1] == "GPE":
+                wrong = get_random_gpe()
             elif ner_tags[acomp_idx_ner - 1][1] == "PERSON":
-                # q_type = "Who"
-                wrong = "Bill Nye"
+                # print("Person")
+                wrong = get_random_name()
+                # print(wrong)
             elif ner_tags[acomp_idx_ner - 1][1] == "DATE":
                 wrong = get_diff_date(ner_tags[acomp_idx_ner - 1][0])
             elif ner_tags[acomp_idx_ner - 1][1] == "LOC":
                 # q_type = "Who"
-                wrong = "America"
+                wrong = get_random_loc()
             elif ner_tags[acomp_idx_ner - 1][1] == "QUANTITY":
                 # q_type = "Who"
-                wrong = "42"
+                wrong = get_random_number()
             elif ner_tags[acomp_idx_ner - 1][1] == "MONEY":
                 # q_type = "Who"
                 wrong = "$42"
             elif ner_tags[acomp_idx_ner - 1][1] == "PERCENT":
                 # q_type = "Who"
                 wrong = "42%"
+        else:
+            return []
         # q = "Is " + ner_tags[acomp_idx_ner - 1][0] + " " + " ".join(passed_tokens) + " or " + wrong + "?"
-        ner_only = util_service.get_ner(sentence)
-        print(ner_only)
-        q = "Is " + ner_only[0][0] + " " + " ".join(passed_tokens) + " or " + wrong + "?"
+        
+        # print(ner_only)
+        q = acomp_word + " " + ner_only[0][0] + " or " + wrong + " " + substance_of_sent
         print(q)
         return [q]
     except:
@@ -107,14 +178,16 @@ def generate_question(sentence):
 # generate_question('For this reason, the Old Kingdom is frequently referred to as "the Age of the Pyramids."')
 # generate_question("A member of the inaugural class of the U.S. Soccer residency program in Bradenton, Florida, Donovan was declared player of the tournament for his role in the United States U17 squad that finished fourth in the 1999 FIFA U-17 World Championship.")
 # generate_question("In Major League Soccer, Donovan won a record six MLS Cups and is both the league's all-time top scorer with 144 goals and the league's all-time assists leader with 136.")
-# generate_question("Donovan was born on March 4, 1982, in Ontario, California, to Donna Kenney-Cash, a special education teacher, and Tim Donovan, a semi-professional ice hockey player originally from Canada, which makes Donovan a Canadian citizen by descent. His mother raised him and his siblings in Redlands, California.")
+# generate_question("His mother raised him and his siblings in Redlands, California.")
 # generate_question("The Galaxy had another successful campaign in 2010 winning the Supporters' Shield for the first time since 2003.")
 # generate_question("Donovan married actress Bianca Kajlich on December 31, 2006; the couple separated in July 2009, and Donovan filed for divorce in December 2010.")
-# # generate_question("In 1997, Alex Jones moved to Sporting CP.")
-# # generate_question("In 2003 Alex Jones signed for Manchester United for £12.2 million (€15 million).")
+# generate_question("In 1997, Alex Jones moved to Sporting CP.")
+# generate_question("In 2003 Alex Jones signed for Manchester United for £12.2 million (€15 million).")
 # generate_question("His buyout clause is valued at €1 billion.")
-
-
+# generate_question("On September 18, 2010, Alex Jones scored an equalizing goal on 56 minutes with a header against Blackburn Rovers at Ewood Park in the 1–1 draw to continue Fulham's unbeaten record in the Barclays Premier League.")
+# generate_question("English is the official language of China and Taiwan, as well as one of four official languages of Singapore.")
+# generate_question("The Clan is a bad organization")
+# generate_question("Evan Kaaret is worth 12 dollars.")
 
 
 
